d48188cb46ec93ad809bcfb6699b2c2ad8758270
Daniel Kessler <dkessler@ocf.berkeley.edu>
2018-04-18 12:13:03 -0700

    grafana auth proxy

--- modules/ocf_stats/manifests/grafana.pp
+++ modules/ocf_stats/manifests/grafana.pp
@@ -4,22 +4,42 @@ class ocf_stats::grafana {
     stage => first,
   }
 
-  #package { 'grafana':; }
-
   class { 'grafana':
     manage_package_repo => false,
 
     cfg                 => {
-      app_mode => 'production',
-      server   => {
+      app_mode     => 'production',
+      server       => {
         http_addr => '127.0.0.1',
         http_port => 8990,
 
+        domain    => 'leprosy.ocf.berkeley.edu',
         root_url  => 'https://leprosy.ocf.berkeley.edu/grafana/',
       },
-      database => {
+      database     => {
         type => 'sqlite3',
         path => 'grafana.db',
+      },
+      users        => {
+        auto_assign_org      => true,
+        allow_sign_up        => false,
+        auto_assign_org_role => 'Editor',
+      },
+      'auth'       => {
+        disable_login_form   => true,
+        disable_signout_menu => true,
+      },
+      'auth.proxy' => {
+        enabled         => true,
+        header_name     => 'X-WEBAUTH-USER',
+        header_property => 'username',
+        auto_sign_up    => true,
+      },
+      'auth.basic' => {
+        enabled => false,
+      },
+      security     => {
+        disable_gravatar => true,
       },
     },
   }
